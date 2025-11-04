b974effc43917b154155f7ec69ecc68c918a1206
alacherif10 <73743207+alacherif10@users.noreply.github.com>
2025-11-04 15:48:28 +0100

    Update register.service.ts

--- frontend/src/app/services/RegisterService/register.service.ts
+++ frontend/src/app/services/RegisterService/register.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RegisterService {
-  private signupUrl = 'http://35.222.253.93:8080/api/auth/register';
+  private signupUrl = 'http://abbf02380dd7a49f28ea61f8c24ef0d1-1327904513.us-east-2.elb.amazonaws.com/api/auth/register';
 
   constructor(private http: HttpClient) { }
 
